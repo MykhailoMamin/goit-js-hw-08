cfadc55fb684c172b23dbbbc3acdcf012b7b8a6c
MykhailoMamin <mishamamin2016@gmail.com>
2023-08-20 20:09:16 +0300

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,25 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const ulEl = document.querySelector('.gallery');
+
+const createMarkupGallery = galleryItems
+  .map(
+    item =>
+      `<li class="gallery__item">
+   <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+   </a>
+</li>`
+  )
+  .join('');
+
+ulEl.insertAdjacentHTML('beforeend', createMarkupGallery);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const formData = [];
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector(`.feedback-form textarea`),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+refs.form.addEventListener('input', onFormInput);
+
+populateTextarea();
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function populateTextarea() {
+  const saveMassage = localStorage.getItem(STORAGE_KEY);
+
+  if (saveMassage) {
+    console.log(saveMassage);
+    refs.textarea.value = saveMassage;
+  }
+}
+
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  console.log(formData);
+  localStorage.setItem('feedback-form', JSON.stringify(formData));
+}
